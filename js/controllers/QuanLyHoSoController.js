d4a241999f7e59455082e7ffb97444fef6c8ce47
madridista1707 <duongduc98@gmail.com>
2022-03-20 10:39:30 +0700

    commit quan ly ho so

---
+++ js/controllers/QuanLyHoSoController.js
@@ -0,0 +1,5 @@
+﻿angular.module('WebApiApp').controller('QuanLyHoSoController', ['$rootScope', '$scope', '$http', '$cookies', '$uibModal', '$settings', function ($rootScope, $scope, $http, $cookies, $uibModal, $settings) {
+   
+  
+
+}]);
